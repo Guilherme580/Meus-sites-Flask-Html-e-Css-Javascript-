bb5a90c25a50119fb14e4a3c61427ec1b7623725
Guilherme Valente /dev <camillog461@gmail.com>
2026-04-12 14:35:55 -0300

    Add files via upload

---
+++ Sites com Python(Flask - django)/Sites_Simples(Flask)/Site03_duas_págs/app.py
@@ -0,0 +1,15 @@
+from flask import *
+
+app = Flask(__name__)
+
+@app.route("/")
+def pagina1():
+    return render_template("principal.html")
+
+@app.route("/pagina2")
+def pagina2():
+    return render_template("secundaria.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
